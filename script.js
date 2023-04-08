fdab2ad7cbfce9e8d9ffdf735d7f3784014897b1
labus-software <labus.software@gmail.com>
2023-04-08 15:46:46 +0200

    Refactoring code and creating classes based on project architecture

--- script.js
+++ script.js
@@ -11,30 +11,136 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// Geolocation - Browser API
-// 1 callback succcess, 2 callback error
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(
-    (position)=>{
-        const {latitude} = position.coords;
-        const {longitude} = position.coords;
-        const coords = [latitude, longitude];
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
-   
-        // we will use LEAFLET - JS Library for Mobile-Friendly Interactive Maps
-        // Leaflet js works for openstreetmap, also for any other kind of maps (Google maps for e.)
-   
-        const map = L.map('map').setView(coords, 13); // first arg are coordinates, second (number 13) is zoom-in out to map
+let map, mapEvent;
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-        
-        L.marker(coords).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
-   
-   
-    }, ()=> alert('Could not get your position Baan, because of permision')
-);
+class Workout {
+  id = (Date.now() + '').slice(-10);
+  date = new Date();
+
+  constructor(coords, distance, duration) {
+    this.distance = distance;
+    this.duration = duration;
+    this.coords = coords;
+  }
 }
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence ) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.pace = this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    return this.duration / this.distance;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain ) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run = new Running([49,-12], 5.2, 24, 178)
+const cyc = new Cycling([49,-12], 27, 95, 522)
+
+console.log(run, cyc)
+///////////////
+// APPLICATION ARCHITECTURE
+
+class App {
+  // private properties
+  #map;
+
+  #mapEvent;
+
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
+  }
+
+  _getPosition() {
+    // Geolocation - Browser API
+    // 1 callback succcess, 2 callback error
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () =>
+        alert('Could not get your position Baan, because of permision')
+      );
+    }
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    // we will use LEAFLET - JS Library for Mobile-Friendly Interactive Maps
+    // Leaflet js works for openstreetmap, also for any other kind of maps (Google maps for e.)
+
+    this.#map = L.map('map').setView(coords, 13); // first arg are coordinates, second (number 13) is zoom-in out to map
+
+    // console.log(map)
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // .on() is built in method of LEAFLET JS
+    // here we will attach event handler, takes 3 params
+    // Params: 1. event 2. callback f 3. options
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+    const { lat, lng } = mapEvent.latlng;
+
+    L.marker([lat, lng]) //create marker based on given coords
+      .addTo(map) // add it to our map
+      .bindPopup(
+        L.popup({
+          // with bind method we will attach container to our marker
+          // container will have all this options below
+          // and will serve as aditional info about the workout
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'cycling-popup',
+        })
+      )
+      .setPopupContent('Go hard or go home') // message that will follow our marker
+      .openPopup(); // function that on click opens our pop up content
+  }
+}
+
+const app = new App();
